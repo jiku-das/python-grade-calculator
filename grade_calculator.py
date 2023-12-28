a36c71cb4ae0e04f2280538458c01d93b7a841c4
jiku-das <dasjiku48@gmail.com>
2023-12-28 11:56:24 +0100

    A grade calculator Python project

---
+++ grade_calculator.py
@@ -0,0 +1,16 @@
+mark = int(input("Please input your mark here: "))
+
+if mark >= 80:
+    print("Your grade is A+")
+elif mark >= 70:
+    print("Your grade is A")
+elif mark >= 60:
+    print("Your grade is A-")
+elif mark >= 50:
+    print("Your grade is B")
+elif mark >= 40:
+    print("Your grade is C")
+elif mark >= 33:
+    print("Your grade is D")
+else :
+    print ("You failed")
